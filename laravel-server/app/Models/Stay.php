4a15c32b5964f4ba15f2e8f068c4c0ea8c1d113d
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-02 03:51:18 +0300

    Creating stay model

---
+++ laravel-server/app/Models/Stay.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class Stay extends Authenticatable implements JWTSubject
+{
+    use HasFactory, Notifiable;
+  
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'date',
+        'rate',
+        'category_id',
+        'image_link'
+    ];
+    
+    
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims() {
+        return [];
+    }    
+}
