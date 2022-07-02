11daf5d36bb77db8b573071519d2377c3344c115
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-02 18:10:05 +0300

    Adding image APIs

---
+++ laravel-server/app/Http/Controllers/ImageController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\Image;
+use Validator;
+
+class ImageController extends Controller{
+      /**
+     * Create a new ProductController instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->middleware('auth:api', ['except' => ['addImage','getImagesByStayId']]);
+    }
+
+    public function getImagesByStayId(Request $request){
+        $image = Image::orderBy('created_at','desc')->get();
+        $image = Image::where('stay_id', $request->stay_id)->get();
+
+            return response()->json([
+                "status" => "Success",
+                "image" => $image
+            ], 200);
+
+    }
+    public function addImage(Request $request){
+        $validator = Validator::make($request->all(), [
+            'image_link' => 'required|string|between:2,1000',
+            'stay_id'=> 'required|string',
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+        $image = Image::create([
+            'image_link' => $request->image_link,
+            'stay_id' => $request->stay_id,
+        ]);
+        return response()->json([
+            'message' => 'image successfully added',
+            'image' => $image
+        ], 201);
+
+    }
+
+
+
+}
