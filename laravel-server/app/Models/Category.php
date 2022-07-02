96a58698764543055e89bf0ca56676ee833c9cc9
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-02 05:40:52 +0300

    Setting up category model

---
+++ laravel-server/app/Models/Category.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class Category extends Authenticatable implements JWTSubject
+{
+    use HasFactory, Notifiable;
+  
+    protected $fillable = [
+        'name',
+    ];
+    
+    
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims() {
+        return [];
+    }    
+}
