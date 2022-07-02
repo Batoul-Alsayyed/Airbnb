4faf885fb8fc2077a27a7c8c86b16953b6eaffe8
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-02 22:21:58 +0300

    Uodating register user API to automatically add user_type attribute to 1 (because we only have admin users in this project)

--- laravel-server/app/Http/Controllers/AuthController.php
+++ laravel-server/app/Http/Controllers/AuthController.php
@@ -43,14 +43,14 @@ class AuthController extends Controller
             'name' => 'required|string|between:2,100',
             'email' => 'required|string|email|max:100|unique:users',
             'password' => 'required|string|min:6',
-            'user_type'=>'required|integer '
         ]);
         if($validator->fails()){
             return response()->json($validator->errors()->toJson(), 400);
         }
         $user = User::create(array_merge(
                     $validator->validated(),
-                    ['password' => bcrypt($request->password)]
+                    ['password' => bcrypt($request->password)],
+                    ['user_type' => 1] 
                 ));
         return response()->json([
             'message' => 'User successfully registered',
