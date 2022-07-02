7e7ac19321652b49e4ef5f03780e06d05aff81f1
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-02 03:50:59 +0300

    Adding stays migration table

---
+++ laravel-server/database/migrations/2022_07_01_232532_create_stays_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stays', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->decimal('price');
+            $table->string('date');
+            $table->decimal('rate');
+            $table->integer('category_id');
+            $table->string('image_link');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stays');
+    }
+}
