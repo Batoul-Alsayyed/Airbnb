2dfd69681f020ab3ebbd9d54e7a8fcea99eb9553
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-02 23:48:33 +0300

    Linking admin login with its API

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import LoginForm from './components/LoginForm';
+import axios from 'axios';
+
 function App() {
   //login testing 
   const adminUser = {
@@ -11,17 +13,36 @@ function App() {
   const [error, setError] = useState("");
 
   const Login = details => {
-    if (details.email === adminUser.email && details.password === adminUser.password){
-      console.log("Logged in");
-      setUser({
-        name: details.name,
-        email: details.email
-      });
-    }else{
-      console.log("Details don't not match");
-      //setError("Details do not match");
-      alert('Credentials do not match')
-    }
+
+    // if (details.email === adminUser.email && details.password === adminUser.password){
+    //   console.log("Logged in");
+    //   setUser({
+    //     email: details.email
+    //   });
+    // }else{
+    //   console.log("Details don't not match");
+    //   //setError("Details do not match");
+    //   alert('Credentials do not match')
+    // }
+    let data = new FormData();
+    data.append('email', details.email);
+    data.append('password', details.password);
+ 
+try{
+  axios.post(`http://127.0.0.1:8000/api/admin/login`, {email: details.email, password: details.password}
+  ).then(res => {
+    console.log(res);
+    //set user email and direct him/her to admins panel
+    setUser({
+          email: details.email
+        });
+  });
+
+}catch(err){
+  setError("Details do not match");
+alert('anauthorized user!')
+}
+  
   }
 
   const Logout = () => {
