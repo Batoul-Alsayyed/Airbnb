e22835213eba1f924cc1c4568a08147e4f0853e1
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-02 18:10:23 +0300

    Creating add like API

--- laravel-server/app/Http/Controllers/StaysController.php
+++ laravel-server/app/Http/Controllers/StaysController.php
@@ -13,7 +13,7 @@ class StaysController extends Controller{
      * @return void
      */
     public function __construct() {
-        $this->middleware('auth:api', ['except' => ['addStay','getAllStays','getStayById']]);
+        $this->middleware('auth:api', ['except' => ['addStay','getAllStays','getStayById','addLike']]);
     }
     public function getAllStays(){
         $stays = Stay::all();
@@ -32,6 +32,26 @@ class StaysController extends Controller{
                 "stay" => $stay
             ], 200);
 
+    }
+    
+    //add like to a stay 
+    public function addLike(Request $request){
+        $stay = Stay::orderBy('created_at','desc')->get();
+        $stay = Stay::where('id', $request->id)->first();
+
+        //getting no of likes for this stay 
+        
+        $number_of_likes = $stay->number_of_likes;
+        
+         $number_of_likes = $number_of_likes + 1; 
+          $stay = $stay->update(['number_of_likes' => $number_of_likes]);
+
+        
+        return response()->json([
+            "status" => "Success",
+            "stay" => $stay,
+            'number of likes' => $number_of_likes,
+        ], 200);
     }
     public function addStay(Request $request){
 
@@ -57,10 +77,6 @@ class StaysController extends Controller{
             'category_id' => $request->category_id,
             'number_of_likes' => 0
         ]);
-        // $stay = Stay::create(array_merge(
-        //             $validator->validated()
-
-        //         ));
         return response()->json([
             'message' => 'stay successfully added',
             'stay' => $stay
