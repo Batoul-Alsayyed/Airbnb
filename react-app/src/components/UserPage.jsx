55205ed5b1b95392e4392c28741b06f6e63d4729
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-03 22:50:45 +0300

    Setting up userpage component

---
+++ react-app/src/components/UserPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Stay from '../components/Stay';
+import "../index.css";
+export default function UserPage() {
+  return (
+    <div>
+      <div className="content-container">
+        <Stay/>
+        <Stay/>
+        <Stay/>
+      </div>
+
+      <div className="content-container">
+          <Stay/>
+      </div>
+      
+
+    </div>
+  )
+}
