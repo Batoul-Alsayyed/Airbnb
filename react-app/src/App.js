6900cdfe024d88cea07e1df66fb18aee63774671
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-03 16:58:48 +0300

    Cleaning the app.js code

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,73 +2,21 @@ import React, {useState} from 'react';
 import LoginForm from './components/LoginForm';
 import Adminpanel from './components/Adminpanel';
 import axios from 'axios';
-
+import { BrowserRouter as Router, Switch, Route, Link, BrowserRouter, Routes } from 'react-router-dom';
+import Mainpage from './components/Mainpage';
 function App() {
-  //login testing 
-  const adminUser = {
-    email: "admin@admin.com",
-    password:"admin123"
-  }
-
-  const [user,setUser] = useState({name:"", email:""});
-  const [error, setError] = useState("");
-
-  const Login = details => {
-
-    // if (details.email === adminUser.email && details.password === adminUser.password){
-    //   console.log("Logged in");
-    //   setUser({
-    //     email: details.email
-    //   });
-    // }else{
-    //   console.log("Details don't not match");
-    //   //setError("Details do not match");
-    //   alert('Credentials do not match')
-    // }
-    let data = new FormData();
-    data.append('email', details.email);
-    data.append('password', details.password);
- 
-try{
-  axios.post(`http://127.0.0.1:8000/api/admin/login`, {email: details.email, password: details.password}
-  ).then(res => {
-    console.log(res);
-    //set user email and direct him/her to admins panel
-    setUser({
-          email: details.email
-        });
-  });
-
-}catch(err){
-  setError("Details do not match");
-alert('anauthorized user!')
-}
   
-  }
-
-  const Logout = () => {
-    console.log("Logout");
-    setUser({name:"", email:""});
-  }
-
   return (
-    <div className="App">
-      {(user.email !="") ? (
-        // <div className='welcome'>
-        <div className='welcome'>
-          
-          {/* load admin panel component */}
-          <Adminpanel></Adminpanel>
-          {/* <button onClick={Logout}>Logout</button> */}
-          </div>
-      ) : (
-       
-         <LoginForm Login={Login} error={error}></LoginForm>
-        
-    
-      )}
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<LoginForm/>}></Route>
+        <Route path='/user' element={<Mainpage/>}></Route>
+        <Route path='/admin' element={<Adminpanel/>}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
-  );
-}
+  )
 
+}
 export default App;
