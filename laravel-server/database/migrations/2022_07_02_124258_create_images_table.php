f82e70d2a6514c275e4c56d57581f53ed4c07635
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-03 16:55:44 +0300

    Changing stay id to integer

--- laravel-server/database/migrations/2022_07_02_124258_create_images_table.php
+++ laravel-server/database/migrations/2022_07_02_124258_create_images_table.php
@@ -16,7 +16,7 @@ class CreateImagesTable extends Migration
         Schema::create('images', function (Blueprint $table) {
             $table->id();
             $table->string('image_link');
-            $table->string('stay_id');
+            $table->integer('stay_id');
             $table->timestamps();
         });
     }
