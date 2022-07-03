e6f98300d9bc6ec7fc8acb8b5cd37f992a0506f7
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-03 22:49:39 +0300

    Adding subnavbar and userpage components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,9 +3,9 @@ import LoginForm from './components/LoginForm';
 import Adminpanel from './components/Adminpanel';
 import { BrowserRouter as Router, Switch, Route, Link, BrowserRouter, Routes } from 'react-router-dom';
 import Mainpage from './components/Mainpage';
-
+import SubNavBar from './components/SubNavBar';
 import UserNavBar from './components/UserNavBar';
-
+import UserPage from './components/UserPage'
 function App() {
   
   return (
@@ -16,6 +16,8 @@ function App() {
         <Route path='/user' element={<Mainpage/>}></Route>
         <Route path='/admin' element={<Adminpanel/>}></Route>
         <Route path='/navbar' element={<UserNavBar/>}></Route>
+        <Route path='/subnavbar' element={<SubNavBar/>}></Route>
+        <Route path='/userpage' element={<UserPage/>}></Route>
       </Routes>
       </BrowserRouter>
     </div>
