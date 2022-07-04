70a9b444a0ef04c4ea0770356dd88d2f968b2269
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-04 07:07:42 +0300

    Linking stay component with its API

--- react-app/src/components/Stay.jsx
+++ react-app/src/components/Stay.jsx
@@ -1,32 +1,44 @@
 import React from 'react'
-
+import axios from "axios";
 import { FaStar } from "react-icons/fa";
 import { FaRegHeart } from "react-icons/fa";
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-export default function Stay() {
+export default function Stay({rate,name,description,price, date, id}) {
+    const [img, setImg] = useState("")
+    useEffect(()=>{
+        axios.post(`http://127.0.0.1:8000/api/admin/getImagesByStayId`, {stay_id: {id}})
+        .then(res => {
+    
+          setImg(res.data['image'][0].image_link);
+        });
+
+    }, []);
+    
   return (
     <div className="content-stays">
             <div className="stays-item">
-                <img src="https://a0.muscache.com/im/pictures/765acada-1030-4bf4-908d-1c9e8f0b95bf.jpg" alt=""
-                />
+                <img src={img} />
+
                 <span className='icon'><FaRegHeart/></span>
                 <div>
-                    <p>Name</p>
-                    <span className='rate'>Rate
+                    <p>{name}</p>
+                    <span className='rate'>{rate}
                     <FaStar/>
                     </span>
                     </div>
 
                 <div>
-                    <p>description</p>
+                    <p>{description}</p>
                 </div>
 
                 <div>
-                    <p>date</p>
+                    <p>{date}</p>
                 </div>
 
                 <div>
-                    <p>price per night</p>
+                    <p>{price}</p>
                 </div>
 
                 </div>
--- react-app/src/components/UserPage.jsx
+++ react-app/src/components/UserPage.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import Stay from '../components/Stay';
+import React, { useState, useEffect } from "react";
+import Stay from "../components/Stay";
 import "../index.css";
+import axios from "axios";
+import UserNavBar from './UserNavBar'
+import SubNavBar from './SubNavBar'
 export default function UserPage() {
+  //loading stays from database:
+  const [stays, setStays] = useState([]);
+  useEffect(() => {
+    const staysData = async ()=> { 
+
+      const req = await fetch(`http://127.0.0.1:8000/api/admin/stays`)
+      const formatData = await req.json()
+      //console.log(formatData);
+      setStays(formatData.stays)
+    } 
+    staysData()
+  }, [])
   return (
-    <div>
+<div>
+  <UserNavBar/>
+<SubNavBar/>
+    {/* <div id="stays">
       <div className="content-container">
-        <Stay/>
-        <Stay/>
-        <Stay/>
+        {stays.map((stay,index)=> 
+          <Stay id={stay.id} key={index} name={stay.name} 
+          description={stay.description} 
+          rate={stay.rate} date={stay.date} 
+          price={stay.price}/>
+          )}
       </div>
-
-      <div className="content-container">
-          <Stay/>
-      </div>
-      
-
+    </div> */}
     </div>
-  )
+  );
 }
